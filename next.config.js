47cf09ad7476b8fe7055065bbf22f493eff46889
Sorpresa <gerorubio@outlook.com>
2022-12-12 12:01:21 -0600

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack: function (config, options) {
+		config.experiments = {
+			asyncWebAssembly: true,
+			layers: true,
+		}
+		return config
+	},
 }
 
 module.exports = nextConfig
