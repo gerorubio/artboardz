dce7a4c3a889e181f7f619eca2915f1f604a7537
Sorpresa <gerorubio@outlook.com>
2022-12-12 21:00:11 -0600

    Table for users

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/admin/users',
+        basePath: false,
+        permanent: false
+      }
+    ]
+  },
+  webpack: function (config, options) {
+		config.experiments = {
+			asyncWebAssembly: true,
+			layers: true,
+		}
+		return config
+	},
 }
 
 module.exports = nextConfig
