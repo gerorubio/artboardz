6fccf432c8d7869b9bcfc38331d237aeb59fcd7e
Sorpresa <gerorubio@outlook.com>
2022-12-14 16:10:54 -0600

    Backgrtound fix, font add, leaderboard init

--- pages/leaderboardz/index.tsx
+++ pages/leaderboardz/index.tsx
@@ -1,6 +1,33 @@
-import { Container, Typography, Box, Stack } from "@mui/material";
+import { Container, Typography, Box, Stack, Grid, Avatar } from "@mui/material";
 import { flexbox } from "@mui/system";
 
+const dataTmp = [
+  {
+    'src': "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    'name': 'The Art Bank ',
+    'size': 20
+  },
+  {
+    'src': "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    'name': 'Aaron Arfmani',
+    'size': 18
+  },
+  {
+    'src': "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    'name': 'Vitaliy Kydon',
+    'size': 15
+  },
+  {
+    'src': "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    'name': 'Random Guy',
+    'size': 10
+  },
+  {
+    'src': "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    'name': 'Nice Girl',
+    'size': 8
+  }
+]
 
 export default function Leaderboardz() {
   
@@ -22,6 +49,16 @@ export default function Leaderboardz() {
           <Typography align="center">5</Typography>
         </Stack>
       </Box>
+      {/* <Grid container rowSpacing={1}>
+        {
+          dataTmp.map((row, index) =>
+            <Grid item>{ index + 1 }</Grid>
+            <Grid item><Avatar src={ row.src } /></Grid>
+            <Grid item>{ row.name }</Grid>
+            <Grid item>{ row.size }</Grid>
+          )
+        }
+      </Grid> */}
     </Container>
   );
 }
